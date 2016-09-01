bd9becd399c2865c6abac4d18b476df136d9e096
EversonMatos <everson.willy@gmail.com>
2016-09-01 20:52:09 -0300

    cena certa das portas

---
+++ Assets/Scripts/CHAVE.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+[RequireComponent(typeof(AudioSource))]
+public class CHAVE : MonoBehaviour {
+	public int IDdaChave;
+	public float DistanciaDaChave = 3;
+	public AudioClip somChave;
+	private bool PegouChave;
+	private GameObject Jogador;
+	void Start (){
+		PegouChave = false;
+		Jogador = GameObject.FindWithTag ("Player");
+	}
+	void Update () {
+		if (Vector3.Distance (transform.position, Jogador.transform.position) < DistanciaDaChave) {
+			if (Input.GetKeyDown ("e") && PegouChave == false) {
+				PORTA.ListaDeIDs.Add (IDdaChave);
+				PegouChave = true;
+				GetComponent<MeshRenderer> ().enabled = false;
+				GetComponent<AudioSource> ().PlayOneShot (somChave);
+				Destroy (gameObject,2.0f);
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/PASSOS.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+[RequireComponent(typeof(AudioSource))] 
+[RequireComponent(typeof(CharacterController))] 
+public class PASSOS : MonoBehaviour {
+	public AudioClip Madeira;
+	private CharacterController controller;
+	private bool Pulou,Esperando,EstaNaAgua;
+	private float TempoDeEspera,tempoCorridaENormal = 1;
+	public float TempoMadeira = 0.6f;
+
+	void Start (){
+		controller = GetComponent<CharacterController> ();
+	}
+
+	void Update (){
+		GetComponent<AudioSource>().clip = Madeira;
+		GetComponent<AudioSource>().PlayOneShot (GetComponent<AudioSource>().clip);
+		}
+	}
