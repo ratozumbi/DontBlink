55806827af1e792eb51993b7b8122157a1470edb
RatoZumbi <brunowilliandrade@gmail.com>
2016-10-03 01:21:29 -0300

    add script de trigger

---
+++ Assets/Scripts/TriggerScene.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class TriggerScene : MonoBehaviour {
+
+	private bool jobDone = false;
+	public string sceneToLoad = "";
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (!jobDone && other.tag == "Player") {
+			jobDone = true;
+			SceneManager.LoadScene(sceneToLoad);
+
+		}
+	}
+}
