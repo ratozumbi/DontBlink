4183c1e17d49c88d16f0b6e5ff921f2e33135c5f
EversonMatos <everson.willy@gmail.com>
2016-10-14 13:45:14 -0300

    Adicionei o som ambiente nas 3 cenas e na ultima, coloquei um tipo de susto proximo a mesa com a caveira

---
+++ Assets/Scripts/AUDIOS.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+[RequireComponent(typeof(AudioSource))]
+public class AUDIOS : MonoBehaviour {
+	private BoxCollider[] Colisores;
+	public AudioClip Audio;
+	void Start () {
+		Colisores = gameObject.GetComponents<BoxCollider> ();
+		GetComponent<AudioSource>().clip = Audio;
+	}
+	void OnTriggerEnter (){
+		GetComponent<AudioSource>().PlayOneShot (GetComponent<AudioSource>().clip);
+		foreach (BoxCollider BoxColl in Colisores) {
+			BoxColl.enabled = false;
+		}
+		Destroy (gameObject, GetComponent<AudioSource>().clip.length);
+	}
+}
